b5bac574d30ff48d279a443815673fcd17c8b0e1
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-03 12:49:10 +0700

    Coba SWITCH..CASE

--- src/Latihan/Latihan1.java
+++ src/Latihan/Latihan1.java
@@ -64,6 +64,15 @@ public class Latihan1 {
                   System.out.println("nilai lebih dari 2");
                   if(a < 4){
                       System.out.println("nilai kurang dari 4");  
+                     System.out.println("\nSWITCH..CASE");
+                     switch (a){
+                         case 1:  System.out.println("nilai a => 1"); break;
+                         case 2:  System.out.println("nilai a => 2"); break;
+                         case 3:
+                         case 4:  System.out.println("nilai a => 3 atau 4"); break;
+                         case 5:  System.out.println("nilai a => 5"); break;
+                         default: System.out.println("nilai a bukan antara 1-5");
+                     }
                   }
                   }
                 }
